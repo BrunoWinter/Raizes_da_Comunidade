0e264d7d1e92507f1a5dadced416aa756ff2fa06
cristielle <cristielle.santos94@gmail.com>
2024-04-02 11:34:54 -0300

    feat: criando primeira versao da home

--- setup/settings.py
+++ setup/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
