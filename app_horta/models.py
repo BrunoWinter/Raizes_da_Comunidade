11ef4d2e2061e5a05e496289ef550cdfa49060d5
Luiz Prudente <luiz.prudente147@gmail.com>
2024-04-10 00:04:06 -0300

    Criação das Models que irão interagir com a Atividade e com a Horta, possiveis alterações no tipo de relacionamento

--- app_horta/models.py
+++ app_horta/models.py
@@ -1,12 +1,26 @@
 from django.db import models
-
 from django.db import models
 from django.contrib.auth.models import User
 
 class Horta(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    nome = models.CharField(null= True,max_length=50)
-    descricao = models.TextField(null= True,max_length=150)
+    nome = models.CharField(null= False, max_length=50)
+    descricao = models.TextField(null= False, max_length=150)
 
     def __str__(self):
-        return f"self.nome - self.descricao"
+        return f"self.nome"
+    
+    
+class Cultura(models.Model):
+    horta = models.ForeignKey(Horta, on_delete=models.CASCADE)
+    nome = models.CharField(null= False, max_length=50)
+    descricao = models.TextField(null= False, max_length=150)
+    tipo = models.CharField(null= False, max_length=50)
+    
+class Plantio(models.Model):
+    cultura = models.ForeignKey(Cultura, on_delete=models.CASCADE)
+    dt_plantio = models.DateField(null=False)
+    ultima_atv = models.DateField(null=True)
+    descricao_atv = models.TextField(null= True)
+    
+
