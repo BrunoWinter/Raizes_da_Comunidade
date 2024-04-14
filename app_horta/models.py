c6ee2c2cdad67429ca22f0a112f9402e18753e36
Luiz Prudente <luiz.prudente147@gmail.com>
2024-04-14 02:09:37 -0300

    Criação das Culturas, inserção no bd e exibição nos templates

--- app_horta/models.py
+++ app_horta/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Horta(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     nome = models.CharField(null= False, max_length=50)
-    descricao = models.TextField(null= False, max_length=150)
+    descricao = models.CharField(max_length=150)
 
     def __str__(self):
         return f"self.nome"
@@ -13,14 +13,17 @@ class Horta(models.Model):
     
 class Cultura(models.Model):
     horta = models.ForeignKey(Horta, on_delete=models.CASCADE)
-    nome = models.CharField(null= False, max_length=50)
-    descricao = models.TextField(null= False, max_length=150)
-    tipo = models.CharField(null= False, max_length=50)
+    name = models.CharField(null= False, max_length=50)
+    type = models.CharField(max_length=50)
+    desc = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return f"{self.name} - {self.type} - {self.desc}"
     
 class Plantio(models.Model):
     cultura = models.ForeignKey(Cultura, on_delete=models.CASCADE)
     dt_plantio = models.DateField(null=False)
     ultima_atv = models.DateField(null=True)
-    descricao_atv = models.TextField(null= True)
+    descricao_atv = models.CharField(max_length=150,null= True)
     
 
--- app_horta/urls.py
+++ app_horta/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from app_horta.views import criar_horta, minha_horta
+from app_horta.views import criar_horta, minha_horta, inserir_cultura, excluir_cultura
 
 urlpatterns = [
     path('horta/', minha_horta, name='minha_horta'),
-    path('horta/criar', criar_horta, name='criar_horta')
+    path('horta/criar', criar_horta, name='criar_horta'),
+    path('horta/inserir/', inserir_cultura, name='inserir_cultura')
 ]
