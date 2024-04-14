bb629ecc55c4ed65eb5fac20f9d4676a7b610514
Luiz Prudente <luiz.prudente147@gmail.com>
2024-04-14 02:11:46 -0300

    Retirando importação de futura feature que havia quebrado o codigo

--- app_horta/urls.py
+++ app_horta/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from app_horta.views import criar_horta, minha_horta, inserir_cultura, excluir_cultura
+from app_horta.views import criar_horta, minha_horta, inserir_cultura
 
 urlpatterns = [
     path('horta/', minha_horta, name='minha_horta'),
